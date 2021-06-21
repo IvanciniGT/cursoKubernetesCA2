942d76ef76e1d8adb52fe2d738037814a6533bda
Ubuntu <ubuntu@ip-172-31-11-196.eu-west-1.compute.internal>
2021-06-21 08:45:39 +0000

    vols

--- despliegues/volumenesES.sh
+++ despliegues/volumenesES.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+kubectl delete pv master-1
+kubectl delete pv master-2
+kubectl delete pv master-3
 
-../instalacion/volumen.sh -n master-1 -c redundante -s 5Gi -p /home/ubuntu/enviroment/datos/es/master1
-../instalacion/volumen.sh -n master-2 -c redundante -s 5Gi -p /home/ubuntu/enviroment/datos/es/master2
-../instalacion/volumen.sh -n master-3 -c redundante -s 5Gi -p /home/ubuntu/enviroment/datos/es/master3
+../instalacion/volumen.sh -n master-1 -c redundante -s 5Gi -p /home/ubuntu/environment/datos/es/master1
+../instalacion/volumen.sh -n master-2 -c redundante -s 5Gi -p /home/ubuntu/environment/datos/es/master2
+../instalacion/volumen.sh -n master-3 -c redundante -s 5Gi -p /home/ubuntu/environment/datos/es/master3
